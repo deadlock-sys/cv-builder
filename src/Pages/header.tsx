fef6baf2a51d58840c4b7e091fd770025a09265c
deadlock-sys <125343943+deadlock-sys@users.noreply.github.com>
2023-03-15 21:04:50 +0530

    1 file modified

--- src/Pages/header.tsx
+++ src/Pages/header.tsx
@@ -17,25 +17,25 @@ const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Header() {
-  // const [anchorElNav, setAnchorElNav] =
-  //   (React.useState < null) | (HTMLElement > null);
-  // const [anchorElUser, setAnchorElUser] =
-  //   (React.useState < null) | (HTMLElement > null);
+  const [anchorElNav, setAnchorElNav] =
+    (React.useState < null) | (HTMLElement > null);
+  const [anchorElUser, setAnchorElUser] =
+    (React.useState < null) | (HTMLElement > null);
 
-  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElNav(event.currentTarget);
-  // };
-  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-  // const handleCloseNavMenu = () => {
-  //   setAnchorElNav(null);
-  // };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <AppBar position="static">
