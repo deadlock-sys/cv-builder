0e992535cdff802cb056fd12c66680f6806d8b97
deadlock-sys <125343943+deadlock-sys@users.noreply.github.com>
2023-03-15 20:48:32 +0530

    Footer modified

--- src/Pages/footer.jsx
+++ src/Pages/footer.jsx
@@ -5,27 +5,32 @@ import {
   MDBCol,
   MDBRow,
   MDBIcon,
-  MDBInput
+  MDBInput,
 } from 'mdb-react-ui-kit';
 
 export default function Footer() {
   return (
-    <MDBFooter bgColor='dark' className='text-center text-white text-lg-left'>
-      <MDBContainer className='p-4 pb-0'>
-        <form action=''>
-          <MDBRow className='d-flex justify-content-center'>
-            <MDBCol size='auto' className='mb-4 mb-md-0'>
-              <p className='pt-2'>
+    <MDBFooter bgColor="dark" className="text-center text-white text-lg-left">
+      <MDBContainer className="p-4 pb-0">
+        <form action="">
+          <MDBRow className="d-flex justify-content-center">
+            <MDBCol size="auto" className="mb-4 mb-md-0">
+              <p className="pt-2">
                 <strong>Sign up for our newsletter</strong>
               </p>
             </MDBCol>
 
-            <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
-              <MDBInput type='text' id='form5Example2' label='Email address' contrast />
+            <MDBCol md="5" size="12" className="mb-4 mb-md-0">
+              <MDBInput
+                type="text"
+                id="form5Example2"
+                label="Email address"
+                contrast
+              />
             </MDBCol>
 
-            <MDBCol size='auto' className='mb-4 mb-md-0'>
-              <MDBBtn outline color='light'>
+            <MDBCol size="auto" className="mb-4 mb-md-0">
+              <MDBBtn outline color="light">
                 Subscribe
               </MDBBtn>
             </MDBCol>
@@ -33,9 +38,12 @@ export default function Footer() {
         </form>
       </MDBContainer>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+      >
         &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-white' href='https://mdbootstrap.com/'>
+        <a className="text-white" href="https://mdbootstrap.com/">
           MDBootstrap.com
         </a>
       </div>
@@ -43,4 +51,4 @@ export default function Footer() {
   );
 }
 
-export default Footer;
+//export default Footer;
