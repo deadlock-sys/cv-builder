9754db2a40ef553599447b93ccf288e28f4702c6
deadlock-sys <125343943+deadlock-sys@users.noreply.github.com>
2023-03-15 21:08:27 +0530

    4 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react';
 import { Header, Footer, Home } from './Pages';
-
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 // import reactLogo from './assets/react.svg';
 import './App.css';
 
