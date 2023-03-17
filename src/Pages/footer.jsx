25e1b466e77b4b593d97001e57dbbd28da9a0e43
deadlock-sys <125343943+deadlock-sys@users.noreply.github.com>
2023-03-17 17:30:02 +0530

    1 file modified

--- src/Pages/footer.jsx
+++ src/Pages/footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function Footer() {
-  return <div>lol</div>;
+function Footer() {
+  return(
+  <div>lol</div>
+  )
 }
 
-//export default Footer;
+export default Footer;
