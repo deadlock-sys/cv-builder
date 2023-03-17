bdbfc58e1134151d707a50b9e80053e8b0de305b
deadlock-sys <125343943+deadlock-sys@users.noreply.github.com>
2023-03-17 17:28:13 +0530

    1 file modified

--- src/Pages/footer.jsx
+++ src/Pages/footer.jsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
-  MDBInput,
-  MDBBtn,
-} from 'mdb-react-ui-kit';
 
 export default function Footer() {
   return <div>lol</div>;
